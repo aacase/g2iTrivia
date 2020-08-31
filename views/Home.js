139159e53dbd9277913711882e2ae1c656354085
Aaron Case <aacase@gmail.com>
2020-08-31 15:50:29 -0500

    More Organized Translation Names

--- views/Home.js
+++ views/Home.js
@@ -2,16 +2,15 @@ import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useTranslation } from "react-i18next";
-
 export default function HomeScreen() {
     const navigation = useNavigation();
     const { t } = useTranslation();
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>{t('homeTitle')}</Text>
-        <Text>{t('homeSubtitle')}</Text>
-        <Text>{t('homeChallenge')}</Text>
-        <Button title={t('beginButton')} onPress={() => navigation.navigate("Quiz")}></Button>
+        <Text>{t('home.title')}</Text>
+        <Text>{t('home.subtitle')}</Text>
+        <Text>{t('home.challenge')}</Text>
+        <Button title={t('home.beginButton')} onPress={() => navigation.navigate("Quiz")}></Button>
       </View>
     );
   }
