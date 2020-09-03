ec327be9e9be06b9c256e4f41c5b35f6b66414c4
Aaron Case <aacase@gmail.com>
2020-09-03 16:28:38 -0500

    Home Styles

--- views/Home.js
+++ views/Home.js
@@ -1,16 +1,41 @@
-import * as React from 'react';
-import { View, Text, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import * as React from "react";
+import { View, StyleSheet } from "react-native";
+import { Button, Text } from "@ui-kitten/components";
+import { useNavigation } from "@react-navigation/native";
 import { useTranslation } from "react-i18next";
 export default function HomeScreen() {
-    const navigation = useNavigation();
-    const { t } = useTranslation();
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>{t('home.title')}</Text>
-        <Text>{t('home.subtitle')}</Text>
-        <Text>{t('home.challenge')}</Text>
-        <Button title={t('home.beginButton')} onPress={() => navigation.navigate("Quiz")}></Button>
+  const navigation = useNavigation();
+  const { t } = useTranslation();
+  return (
+    <View style={styles.mainContainer}>
+      <View>
+        <Text category="h3">{t("home.title")}</Text>
+        <Text category="h3">{t("home.titleLineTwo")}</Text>
       </View>
-    );
-  }
+
+      <Text style={styles.middleText} category="h6">
+        {t("home.subtitle")}
+      </Text>
+      <Text style={styles.middleText} category="h6">
+        {t("home.challenge")}
+      </Text>
+      <Button
+        style={styles.button}
+        status="success"
+        onPress={() => navigation.navigate("Quiz")}
+      >
+        {t("home.beginButton")}{" "}
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  middleText: { width: "70%" },
+  button: { marginBottom: 50 },
+});
