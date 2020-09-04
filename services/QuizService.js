ae918e821a6425b6ec710f4e466113844a2e3d8c
Aaron Case <aacase@gmail.com>
2020-09-03 19:25:07 -0500

    Better String Replacement

--- services/QuizService.js
+++ services/QuizService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import {decode} from "he";
 /**
  * This is our data call to retreive questions. On a successful call, we'll take the data and transform it
  * into a usable format for the quiz.
@@ -35,15 +35,13 @@ function transformQuestionObject(quizObject) {
   /**
    * We need to add a field to the question object in order to store the user's
    * answer. This will help us facilitate scorekeeping at the end of the quiz.
-   * We also need to translate the special unicode characters since we're not appending HTML directly.
+   * We also need to translate the special HTML characters since we're not appending HTML directly,
+   * this is done using the "he" library.
    */
   try {
     quizObject.forEach((question) => {
       question.user_answer = null;
-      question.question = question.question
-        .replace(/&quot;/g, '"')
-        .replace(/&#039;/g, "'")
-        .replace(/&epsilon;/g, "ε").replace(/&Phi;/g, "Φ");
+      question.question = decode(question.question)
     });
     return quizObject;
   } catch (err) {
